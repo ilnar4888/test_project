e191102d63db77dba284932e7f17a99ad9b7748d
ilnar4888 <25gaisarov@gmail.com>
2017-11-14 14:41:50 +0300

    add controller event

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,2 +1,23 @@
 class EventController < ApplicationController
+  before_action :authenticate_user!
+  
+  expose :event
+  expose :events, ->{ Event.all.order('created_at DESC') }
+  
+  def index
+  end
+  
+  def new 
+  end
+  
+  def create
+  end
+
+  def show
+  end
+
+  private
+    def store_params
+      params.require(:event).permit(:text, :description, :event_image_id)
+    end
 end
--- db/migrate/20171114112431_create_events.rb
+++ db/migrate/20171114112431_create_events.rb
@@ -1,7 +1,9 @@
 class CreateEvents < ActiveRecord::Migration[5.1]
   def change
     create_table :events do |t|
-
+      t.string :name
+      t.text :description
+      t.string :event_image_id
       t.timestamps
     end
   end
